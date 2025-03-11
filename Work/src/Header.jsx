9e9f69b8e4a718c438cf3ade1b78bdec63a0070a
ExodiaXO <tomer32323@gmail.com>
2025-03-12 01:42:12 +0200

    header commit$

---
+++ Work/src/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+    return (
+    <AppBar>
+    <Toolbar>
+    <Typography variant="h6" component="div">
+    Header
+    </Typography>
+    </Toolbar>
+    </AppBar>
+    );
+   }
